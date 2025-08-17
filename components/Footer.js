b85b63ddf984ea58255cc136f5384eb2c937e7a9
jordaneaster <jordaneaster51@gmail.com>
2025-08-17 12:55:50 -0400

    remove footer background color

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="bg-gray-900 text-white p-8">
+    <footer className="text-white p-8">
       <div className="container mx-auto text-center">
         <p>&copy; 2023-{currentYear} R&B Versus LIVE. All rights reserved.</p>
       </div>
